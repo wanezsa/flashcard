d3ccb85e758cbe7e44fff8de5b0308a296675f1a
wanezsa <00001130966033sp@al.educacao.sp.gov.br>
2024-11-13 21:42:00 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -11,9 +11,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Curiosidade',
+    'País mais bonito, em questões artisticas?',
+    'A Itália é o país com maior riqueza histoeica e arquitetonica'
 )
 
 criaCartao(
